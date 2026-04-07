56c8f08fd93de584845ad5759a830ed048f7ceac
Drikista <f.joaobeiel@gmail.com>
2026-04-07 15:42:13 -0300

    Revert "Troca de Ingredientes"
    This reverts commit 682b4353fc6175236de3d300ce802f780aecf47e.

--- Assets/_Game/Scripts/NextIngredient.cs
+++ Assets/_Game/Scripts/NextIngredient.cs
@@ -4,9 +4,7 @@ public class NextIngredient : MonoBehaviour
 {
 
 //criar variavel do objeto com script ingrediente
-public IngredientBehaviour Ingrediente;
-
-
+public GameObject Ingrediente;
 
 //associar o objeto
 //ja feito
@@ -14,40 +12,7 @@ public IngredientBehaviour Ingrediente;
 
 //acessar o script do ingrediente
 //quando clica em mim altera para +1 o ID do ingrediente
-void OnMouseDown()
-   {
-    
-    Ingrediente.myID++;
-    
-    
-    //base
-    if (Ingrediente.myID == 5)
-        {
-            Ingrediente.myID = 1;
-        }
-    
-    //toppings
-    if (Ingrediente.myID == 9)
-        {
-            Ingrediente.myID = 5;
-        }
-    
-    //proteinas
-    if (Ingrediente.myID == 13)
-        {
-            Ingrediente.myID = 9;
-        }
 
-
-    //crunches
-    if (Ingrediente.myID == 17)
-        {
-            Ingrediente.myID = 13;
-        }
-
-    
-    Ingrediente.atualizaIngrediente();
-   }
 
 //roda o metodo de atualializacao do render que esta ingrente
 
@@ -59,7 +24,7 @@ void OnMouseDown()
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        IngredientBehaviour IB = GetComponent<IngredientBehaviour>();
+        
     }
 
     // Update is called once per frame
