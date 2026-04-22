648aedf9d98ecbaf5342480f4d40fc33dfd9b1e9
Drikista <f.joaobeiel@gmail.com>
2026-04-22 15:27:41 -0300

    Estoque de ingredientes

--- Assets/_Game/Scripts/IngredientBehaviour.cs
+++ Assets/_Game/Scripts/IngredientBehaviour.cs
@@ -5,7 +5,38 @@ public class IngredientBehaviour : MonoBehaviour
     public Sprite[] minhasImagens;
     public int myID;
 
+    public int [] estoque;
 
+void awake()
+{
+for (int i = 0; i < 17; i++)
+    {
+    estoque[i] = 5;
+    }
+}
+
+ public void adicionar(int id, int valor)
+    {
+        estoque[id] += valor;
+    }
+
+    public void remover(int id, int valor)
+    {
+        estoque[id] -= valor;
+
+        if (estoque[id] < 0)
+            estoque[id] = 0;
+    }
+
+    public void removerIngrediente(int valor)
+    {
+    this.remover(myID, valor);
+    }
+
+    public void adicionarIngrediente(int valor)
+    {
+    this.adicionar(myID, valor);
+    }
 public void  atualizaIngrediente(){
 
 SpriteRenderer sr = GetComponent<SpriteRenderer>();
@@ -17,15 +48,8 @@ sr.sprite = minhasImagens[myID];
     {
 
         atualizaIngrediente();
-       
-       // if (sr != null && myID >= 0 && myID < minhasImagens.Length)
-       // {
-    
-      // }
-      //  else
-      //  {
-      //      Debug.LogWarning("SpriteRenderer não encontrado ou ID inválido");
-       // }
+
     }
+
 
 }
